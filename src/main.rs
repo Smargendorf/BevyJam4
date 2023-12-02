8ad89e3ae219db1b56974d7986a4453655fb429b
Ryan Chomistek <ryan.chomistek@gmail.com>
2023-12-02 15:29:56 -0800

    fixing naming on constants

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 const PLAYER_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 const ANT_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 
-const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 1.0);
-const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+const CAMERA_STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const ANT_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
 
 const TIME_SCALE: f32 = 2.0;
 
@@ -34,19 +34,18 @@ fn setup(
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::default().into()).into(),
             material: materials.add(ColorMaterial::from(PLAYER_COLOR)),
-            transform: Transform::from_translation(BALL_STARTING_POSITION).with_scale(BALL_SIZE),
+            transform: Transform::from_translation(CAMERA_STARTING_POSITION).with_scale(ANT_SIZE),
             ..default()
         },
-        components::Player,
+        Player,
     ));
 
     for _ in 0..10
     {
-        let mass = (rng.next_u32() as i32 % 200 + 15) as f32;
         let transform = Transform::from_translation(Vec3::new(
             (rng.next_u32() as i32 % 500) as f32,
             (rng.next_u32() as i32 % 500) as f32,
-            1.0)).with_scale(Vec3::new(mass, mass, 0.0));
+            1.0)).with_scale(ANT_SIZE);
 
         commands.spawn((
             MaterialMesh2dBundle {
@@ -55,7 +54,7 @@ fn setup(
                 transform: transform,
                 ..default()
             },
-            components::Ant
+            Ant
         ));
     }
 }
