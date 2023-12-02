dd1724d4638fa1a85113cb6b789df2665541826c
smargendorf <eldar825@gmail.com>
2023-12-02 15:32:58 -0800

    adding food resource

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use bevy::input::mouse::MouseWheel;
-use bevy::{
-    prelude::*,
-    sprite::MaterialMesh2dBundle,
-};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
+use bevy_prng::ChaCha8Rng;
 use bevy_rand::prelude::*;
 use rand_core::RngCore;
-use bevy_prng::ChaCha8Rng;
 
 mod camera;
 mod components;
+mod food;
 
-use self::components::*;
-use self::camera::*;
+use camera::*;
+use components::*;
+use food::Food;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 const PLAYER_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
@@ -24,11 +23,13 @@ const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
 const TIME_SCALE: f32 = 2.0;
 
 fn setup(
-    mut commands: Commands, 
+    mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut rng: ResMut<GlobalEntropy<ChaCha8Rng>>
+    mut rng: ResMut<GlobalEntropy<ChaCha8Rng>>,
 ) {
+    commands.insert_resource(Food::default());
+
     // Player
     commands.spawn((
         MaterialMesh2dBundle {
@@ -40,13 +41,14 @@ fn setup(
         components::Player,
     ));
 
-    for _ in 0..10
-    {
+    for _ in 0..10 {
         let mass = (rng.next_u32() as i32 % 200 + 15) as f32;
         let transform = Transform::from_translation(Vec3::new(
             (rng.next_u32() as i32 % 500) as f32,
             (rng.next_u32() as i32 % 500) as f32,
-            1.0)).with_scale(Vec3::new(mass, mass, 0.0));
+            1.0,
+        ))
+        .with_scale(Vec3::new(mass, mass, 0.0));
 
         commands.spawn((
             MaterialMesh2dBundle {
@@ -55,20 +57,20 @@ fn setup(
                 transform: transform,
                 ..default()
             },
-            components::Ant
+            components::Ant,
         ));
     }
 }
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(EntropyPlugin::<ChaCha8Rng>::default())
-    .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
-    .insert_resource(ClearColor(BACKGROUND_COLOR))
-    .add_systems(Startup, (setup, camera_setup))
-    .add_systems(Update, (move_player, camera_chase, scroll_events))
-    .add_systems(Update, bevy::window::close_on_esc)
-    .add_event::<MouseWheel>()
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EntropyPlugin::<ChaCha8Rng>::default())
+        .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_systems(Startup, (setup, camera_setup))
+        .add_systems(Update, (move_player, camera_chase, scroll_events))
+        .add_systems(Update, bevy::window::close_on_esc)
+        .add_event::<MouseWheel>()
+        .run();
 }
